e5fec782f87a099cd5a2e2ee3e4b19f149b45842
ttran17 <ttran17@gmail.com>
2013-07-24 19:45:15 -0400

    Adding some utils

---
+++ com/github/ttran17/util/ASMUtils.java
@@ -0,0 +1,30 @@
+package com.github.ttran17.util;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.util.ASMifier;
+import org.objectweb.asm.util.TraceClassVisitor;
+
+public class ASMUtils {
+	public static void main(String[] args) {
+        ClassReader cr;
+		try {
+			String base = "/home/ttran/Projects/forge-1.4.7/mcp/reobf/minecraft/";
+			cr = new ClassReader(new FileInputStream(base + "hn.class"));
+//			cr = new ClassReader(new FileInputStream("axr.class"));
+	        cr.accept(new TraceClassVisitor(null, new ASMifier(), new PrintWriter(
+	                System.out)),  ClassReader.SKIP_DEBUG);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+}
