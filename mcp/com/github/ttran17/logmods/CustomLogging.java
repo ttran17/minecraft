c42c6aaba5cdfe2a8dd6286f6564f0aed7dd06ba
ttran17 <ttran17@gmail.com>
2014-01-25 18:44:01 -0500

    Introducing logmods ...

---
+++ mcp/com/github/ttran17/logmods/CustomLogging.java
@@ -0,0 +1,47 @@
+package com.github.ttran17.logmods;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.minecraft.util.IChatComponent;
+
+public class CustomLogging {
+	
+	private static final Logger LOGGER = LogManager.getLogger();
+
+	/** 
+	 * Called from net.minecraft.command.server.CommandMessage.processCommand()
+	 * 
+	 * @param from
+	 * @param to
+	 * @param message
+	 */
+	public static void logWhisper(IChatComponent from, IChatComponent to, IChatComponent message) {
+		StringBuilder builder = new StringBuilder();
+		builder.append("[CHAT] ");
+		builder.append(from.getUnformattedText());
+		builder.append(" whispers (chats) to ");
+		builder.append(to.getUnformattedText());
+		builder.append(" ");
+		builder.append(message.getUnformattedText());
+		builder.append(": ");
+		
+		LOGGER.info(builder.toString());
+	}
+	
+	/**
+	 * Called from net.minecraft.network.NetHandlerPlayServer.func_147354_a()
+	 * 
+	 * @param from
+	 * @param message
+	 */
+	public static void logChat(IChatComponent from, String message) {
+		StringBuilder builder = new StringBuilder();
+		builder.append("[CHAT] ");
+		builder.append(from.getUnformattedText());
+		builder.append(" ");
+		builder.append(message);
+		
+		LOGGER.info(builder.toString());
+	}
+}
